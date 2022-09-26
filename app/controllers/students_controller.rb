9d9f0ce95d0e52bf4c5bbb1c1ed642a15edc2d69
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-26 20:25:53 +0300

    commit tree 889372a2ca2fb600bff5d26e9c4d54353195afab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
+  # Use query params in the index action to return a list of all students whose first or last name matches the query parameter.
+
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+      else
+        Student.all
+      end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  
 end
